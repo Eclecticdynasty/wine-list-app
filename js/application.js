5ca39026d24c1e151f7ead9d055364b77a0b897a
Eclecticdynastye <bklyngal1994@gmail.com>
2019-04-25 16:08:25 -0400

    more info button for zin,malbec and merlot

--- js/application.js
+++ js/application.js
@@ -2,6 +2,15 @@ $(document).ready(() => {
   $('.pinot-button').on('click', () => {
   $('p.desc-pinot').slideToggle();
  });
+  $('.merlot-button').on('click', () => {
+  $('p.desc-merlot').slideToggle();
+ });
+  $('.zin-button').on('click', () => {
+  $('p.desc-zin').slideToggle();
+ });
+  $('.malbec-button').on('click', () => {
+  $('p.desc-malbec').slideToggle();
+ });
 
   $('.menu-button').on('click',() => {
     $('#nav-dropdown').slideToggle('slow');
