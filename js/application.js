7803c7f99ade17ad3592f4d6d4f03b1ba170eefa
Eclecticdynastye <bklyngal1994@gmail.com>
2019-05-23 19:18:31 -0400

    menu jquery function added back

---
+++ js/application.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+  $('.menu-button').on('click',() => {
+    $('#nav-dropdown').slideToggle('slow');
+  })
+
+
+  // Wine reccomendations
+
+$(function() {
+
+  
+
+
+});
+
+}); 
+  
